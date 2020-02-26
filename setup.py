280f92746c33df4236271d56f6af4a1c2c0de893
jo-moon <joanne.moonsamy@gmail.com>
2020-02-26 11:25:11 +0200

    more bug fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ packages=find_packages(exclude=['tests*']),
 license='MIT',
 description='example python package',
 long_description=open('README.md').read(),
-install_requires=['turtle'],
 url='https://github.com/jo-moon/fluff',
 author='Joanne Moonsamy',
 author_email='joanne.moonsamy@gmail.com'
