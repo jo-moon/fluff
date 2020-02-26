b8510bdfd2f26522e969e8afb0bb9c7c9f0b2f09
jo-moon <joanne.moonsamy@gmail.com>
2020-02-26 11:47:22 +0200

    More bug fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-name='doge',
-version='0.1.1',
-packages=find_packages(exclude=['tests*']),
-license='MIT',
-description='example python package',
-long_description=open('README.md').read(),
-url='https://github.com/jo-moon/fluff',
-author='Joanne Moonsamy',
-author_email='joanne.moonsamy@gmail.com'
+    name='doge',
+    version='0.1.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='example python package',
+    long_description=open('README.md').read(),
+    install_requires=['time','turtle'],
+    url='https://github.com/jo-moon/fluff.git',
+    author='Joanne Moonsamy',
+    author_email='joanne.moonsamy@gmail.com'
 )
