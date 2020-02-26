0ff5c3f94cd2c09dc878002b136df6d844f26585
jo-moon <joanne.moonsamy@gmail.com>
2020-02-26 11:49:38 +0200

    More bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='doge',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='example python package',
